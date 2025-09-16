7225a836bd1272cf4e2a4cb3df0d46e88b4304c9
PADETI NAVEEN <163728329+naveenpadeti@users.noreply.github.com>
2025-09-16 12:46:53 +0530

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,7 +3,7 @@ import { useCart } from "../context/CartContext";
 import { useNavigate } from "react-router-dom";
 import "./style.css";
 
-const BASE_URL = 'http://localhost:9090/back1';
+const BASE_URL = '44.211.224.216:8083/back1';
 
 const Cart = () => {
   const { cartItems, removeFromCart } = useCart();
